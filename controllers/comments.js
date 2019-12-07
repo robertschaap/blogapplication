50d1cb7cc155699940abf25edee70d7d266d961b
robertschaap <robert.schaap@mac.com>
2019-12-07 21:50:37 +0100

    comments: linting fixes

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,9 +6,9 @@ const model = require('../models');
 // Comments Routes
 router.post('/new/:id', (req, res) => {
   if ( req.session && req.body.comment ) {
-    model.Comments.createComment( req.params.id, req.session, req.body) .then(() => {
+    model.Comments.createComment(req.params.id, req.session, req.body).then(() => {
       res.redirect(`/posts/${req.params.id}`);
-    })
+    });
   } else {
     res.redirect(`/posts/${req.params.id}`);
   }
